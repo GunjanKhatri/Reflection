6eb6f81762641ef2f3bd319562728df39ccd9532
Gunjan Advani <Gunjan.Advani@shell.com>
2022-08-17 19:12:48 +0530

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EMSEntities ems =new EMSEntities()){
+                Console.WriteLine("Please enter search field");
+                var searchField = Console.ReadLine();
+                Console.WriteLine("Please enter search value");
+                var searchValue = Console.ReadLine();
+
+                var result = ExtractInfo.GetByCodi<Employee>(ems.Employees, searchValue, searchField);
+
+                
+                
+                    Console.WriteLine(result.FirstName);
+                    Console.WriteLine(result.LastName);
+                    Console.WriteLine(result.State);
+                    Console.WriteLine(result.City);
+                
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
